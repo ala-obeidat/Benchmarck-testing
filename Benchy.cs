ee8e22a2bd7328019791afc2c151d78e794254de
Ala Obeidat <ala-work@hotmail.com>
2021-08-01 10:25:34 +0300

    Add code

---
+++ Benchy.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using System;
+
+namespace Testing
+{
+    [MemoryDiagnoser]
+    public class Benchy
+    {
+        private readonly Guid[] _input = new Guid[100000];
+
+        [Benchmark]
+        public void ForLoop()
+        {
+            for (int i = 0; i < _input.Length; i++)
+            {
+                Guid.NewGuid();
+            }
+        }
+
+        [Benchmark]
+        public void ForEarch()
+        {
+            foreach (Guid item in _input)
+            {
+                Guid.NewGuid();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Testing
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchy>();
+        }
+    }
+
+
+}
